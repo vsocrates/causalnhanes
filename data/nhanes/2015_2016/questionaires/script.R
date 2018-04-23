0aec19f268dad4ab3f7ca730d762a96ff92d5f1b
assortedletters <assortedletters@gmail.com>
2018-04-23 12:02:12 -0400

    r script

--- data/nhanes/2015_2016/questionaires/script.R
+++ data/nhanes/2015_2016/questionaires/script.R
@@ -6,5 +6,13 @@ depScore[2] = cleanD[3]
 for(i in 4:11) {depScore[2] = depScore[2] + cleanD[i]}
 goodSleep = merge(cleanSleep, depScore, type = "inner")
 badSleep = merge(cleanSleep, depScore, type = "inner")
-goodSleep = goodSleep %>% filter(SLQ120 < 3)
-badSleep = badSleep %>% filter(SLQ120 > 2)
+#goodSleep = goodSleep %>% filter(SLQ120 < 3)
+#badSleep = badSleep %>% filter(SLQ120 > 2)
+
+depSleepy = goodSleep %>% filter(SLQ120 > 2)
+hapSleepy = goodSleep %>% filter(SLQ120 > 2)
+depRest = goodSleep %>% filter(SLQ120 <3)
+hapRest = goodSleep %>% filter(SLQ120 <3)
+
+depSleepy = depSleepy %>% filter(DPQ010 > 9)
+hapSleepy = hapSleepy %>% filter(DPQ010 < 9)
