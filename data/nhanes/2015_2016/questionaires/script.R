9790b9cf3bb4661f423e720e5da868cf231db789
assortedletters <assortedletters@gmail.com>
2018-04-23 15:11:05 -0400

    Added an R notebook

--- data/nhanes/2015_2016/questionaires/script.R
+++ data/nhanes/2015_2016/questionaires/script.R
@@ -6,8 +6,6 @@ depScore[2] = cleanD[3]
 for(i in 4:11) {depScore[2] = depScore[2] + cleanD[i]}
 goodSleep = merge(cleanSleep, depScore, type = "inner")
 badSleep = merge(cleanSleep, depScore, type = "inner")
-#goodSleep = goodSleep %>% filter(SLQ120 < 3)
-#badSleep = badSleep %>% filter(SLQ120 > 2)
 
 depSleepy = goodSleep %>% filter(SLQ120 > 2)
 hapSleepy = goodSleep %>% filter(SLQ120 > 2)
@@ -16,3 +14,5 @@ hapRest = goodSleep %>% filter(SLQ120 <3)
 
 depSleepy = depSleepy %>% filter(DPQ010 > 9)
 hapSleepy = hapSleepy %>% filter(DPQ010 < 9)
+depRest = depRest %>% filter(DPQ010 > 9)
+hapRest = hapRest %>% filter(DPQ010 < 9)
